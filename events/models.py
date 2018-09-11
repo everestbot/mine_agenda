e5493a918ac3d4082bc74d5045c414559a447e70
everestbot <emanuel.everest@gmail.com>
2018-09-11 20:52:12 -0300

    Criando tabela de eventos

--- events/models.py
+++ events/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Event(models.Model):
 
     priorities_list = (
-        ('0','Sem prioridade')
-        ('1','Normal')
-        ('2','urgente')
-        ('3','Muito Urgente')
-        ('4','Poxa vem mi ajudar!!! Muito Urgente')
+        ('0','Sem prioridade'),
+        ('1','Normal'),
+        ('2','urgente'),
+        ('3','Muito Urgente'),
+        ('4','Poxa vem mi ajudar!!! Muito Urgente'),
     )
 
     date = models.DateField()
