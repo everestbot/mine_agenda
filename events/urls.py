2975c379c169e8003ff8358d896f485cc9ad0a78
everestbot <emanuel.everest@gmail.com>
2018-09-18 21:37:13 -0300

    Criando navbar e novo evento

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpattners = [
-    path('',views.index, name ='agenda-events-index'),
+urlpatterns = [
+    path('', views.index, name='agenda-events-index'),
 
 ]
