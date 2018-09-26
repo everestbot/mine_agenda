8326652dc80f7f1cb7a6675167659dea71ab3638
everestbot <emanuel.everest@gmail.com>
2018-09-25 21:23:35 -0300

    Resolvendo pequenos erros

--- events/static/events.js
+++ events/static/events.js
@@ -1,15 +1,12 @@
-$ ( documento ). pronto ( function () {
+$(document).ready(function() {
 
-    $ ( ' .edit-event ' ). clique ( function () {
-        / *   recupera o id do objeto que invoca esta rotina e popula os
-            campos de 'id' (oculto), descrição e prioridade. Um dado é
-            preenchida diretamente no gabarito. * /
-        deixe valores =  isso . id . dividir ( ' - ' );
-        $( ' #editInputId ' ). prop ( ' valor ' , valores [ 1 ]);
-        $( ' #editInputEvent ' ). prop ( ' valor ' , este . texto );
-        $( ' #editInputPriority ' ). prop ( ' valor ' , valores [ 2 ]);
+    $('.edit-event').click(function(){
+        /*  recupera o id do objeto que invocou esta rotina e popula os
+            campos de 'id' (oculto), descrição e prioridade. A data é
+            preenchida diretamente no gabarito. */
+        let values = this.id.split('-');
+        $('#editInputId').prop('value', values[1]);
+        $('#editInputEvent').prop('value', this.text);
+        $('#editInputPriority').prop('value', values[2]);
 
-        $ ( ' #editEvent ' ). modal ( ' toggle ' );
-    }) // clique ()
-
-}) // function ()
+        $('#editEvent').modal('toggle');
